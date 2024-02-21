f309f14094d68307c89282ca7adb664bde370442
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-02-21 10:41:01 +0300

    ADD LinkedList

---
+++ LinkedList/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        LinkedList<int> linkedList = new LinkedList<int>();
+
+        
+        // Insertion
+        linkedList.AddLast(1);
+        linkedList.AddLast(2);
+        linkedList.AddLast(3);
+
+
+        // Traversal
+        Console.WriteLine("Linked List:");
+        foreach (var item in linkedList)
+        {
+            Console.WriteLine(item);
+        }
+
+        // Deletion
+        linkedList.Remove(2);
+
+        // Search
+        if (linkedList.Contains(3))
+        {
+            Console.WriteLine("Element 3 found.");
+        }
+        else
+        {
+            Console.WriteLine("Element 3 not found.");
+        }
+        Console.ReadKey();
+    }
+}
+
