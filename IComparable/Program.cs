5eb48914cd1ffe1fdd28d90e5fbcde97a00d2ad2
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-02-29 15:47:56 +0300

    ADD IComparable

---
+++ IComparable/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class Person : IComparable<Person>
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+
+    // Implementing the CompareTo method for Person class
+    public int CompareTo(Person other)
+    {
+        // If other is not a valid object reference, this instance is greater.
+        if (other == null) return 1;
+
+
+        // Use the Age property to determine the order of Person instances.
+        return this.Age.CompareTo(other.Age);
+
+        //// Use the Length of name property to determine the order of Person instances.
+        //return this.Name.Length.CompareTo(other.Name.Length);
+    }
+
+
+    // Constructor for easier initialization
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+
+    // Override ToString to make output easier to read
+    public override string ToString()
+    {
+        return $"{Name}, {Age} years old";
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Creating a list of Person instances
+        List<Person> people = new List<Person>
+        {
+            new Person("John", 30),
+            new Person("Jane", 25),
+            new Person("Doe", 28),
+        };
+
+
+        // Sorting the list using IComparable implementation
+        people.Sort();
+
+
+        // Printing the sorted list
+        Console.WriteLine("People sorted by age:");
+        foreach (Person person in people)
+        {
+            Console.WriteLine(person);
+        }
+
+        Console.ReadKey();
+    }
+}
